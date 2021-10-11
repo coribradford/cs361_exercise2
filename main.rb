30506cbac005fe8b4bd22023a54d0d84d48f0578
cori <bradfcor@oregonstate.edu>
2021-10-10 22:45:04 -0700

    removed returns to maintain ruby convention

--- main.rb
+++ main.rb
@@ -1,18 +1,15 @@
-# Name Mangler
-
 
 def reverse_names(name)
-  return name.split.reverse.join(' ')
+  name.split.reverse.join(' ')
 end
   
 def append_borg_to_name(name)
-  return name << " Borg"
+  name << " Borg"
 end
 
 def modified_name(name)
-  return append_borg_to_name(reverse_names(name))
+  append_borg_to_name(reverse_names(name))
 end
-
 
 puts "New name: #{reverse_names("Johanna Jackson")}"
 puts "New borg name: #{modified_name("Johanna Jackson")}"
